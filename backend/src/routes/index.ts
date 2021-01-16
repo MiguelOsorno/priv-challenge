d858002fa053fa2b07561dd23033d230073b4e19
Miguel Osorno <miguel.osornolinares@gmail.com>
2021-01-15 22:59:42 -0600

    docs(swagger): Add documentation

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,7 +1,11 @@
 import express from 'express';
 import paymentRoutes from './payments.route';
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('../config/swagger.json');
 const router = express.Router();
 
+    
+router.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 router.use('/api/v1', paymentRoutes);
 
 export default router;
