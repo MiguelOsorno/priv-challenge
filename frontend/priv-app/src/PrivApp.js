6aba3d4df117cf3fecac6e939ce5160bcd8ba07a
Miguel Osorno <miguel.osornolinares@gmail.com>
2021-01-16 19:28:02 -0600

    initial commit frontend

---
+++ frontend/priv-app/src/PrivApp.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const PrivApp = () => {
+    return (
+        <div>
+            <h1 className='font-semibold'>Hola mundo</h1>
+        </div>
+    )
+}
---
+++ frontend/priv-app/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { PrivApp } from './PrivApp';
+
+ReactDOM.render(
+    <PrivApp />,
+  document.getElementById('root')
+);
+
